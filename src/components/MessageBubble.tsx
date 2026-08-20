7cce9211480f2ce44634f934b27466dedd2d713f
chenyu.1025 <chenyu.1025@bytedance.com>
2026-08-20 10:57:25 +0800

    feat: show date and time in message bubble

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -27,7 +27,9 @@ function MessageBubble({ message, isCurrentUser, userType, viewerType, onReply }
   }, [message.content, message.type])
 
   const formatTime = (timestamp: string) => {
-    return new Date(timestamp).toLocaleTimeString('zh-CN', {
+    return new Date(timestamp).toLocaleString('zh-CN', {
+      month: '2-digit',
+      day: '2-digit',
       hour: '2-digit',
       minute: '2-digit'
     })
